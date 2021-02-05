c987ed74da791bae7418656c246337c1a4f20a72
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-02-05 16:56:16 +0100

    function __construct

--- tabula_rasa/game.php
+++ tabula_rasa/game.php
@@ -1,2 +1,30 @@
 <?php
+    session_start();
+
+    require_once 'functions/functions.php';
+    require_once 'classes/Autoloader.php';
     
+    Autoloader::register();
+
+    vdp($_REQUEST, '$_REQUEST');
+    vdp($_SESSION, '$_SESSION');
+    $card = new Cards(0, 'A');
+    prp($card, '$card');
+    vdp($card, '$card');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>game</title>
+</head>
+<body>
+    <?php
+        $card->printCard();
+
+    ?>
+    
+</body>
+</html>
--- tabula_rasa/index.php
+++ tabula_rasa/index.php
@@ -6,7 +6,8 @@
     
     Autoloader::register();
 
-    if (isset($_GET['difficulty'])) {
+    if (isset($_REQUEST['difficulty'])) {
+        $_SESSION['difficulty'] = $_REQUEST['difficulty'];
         // $game = new Memory;
         // $game->setDifficulty($_GET['difficulty']);
         // $game->createDeck();
@@ -24,7 +25,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="styles/styleG.css">
-        <title>index</title>
+        <title>choix de la difficulté</title>
     </head>
     <body>
         <header>
