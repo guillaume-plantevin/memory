b4480c466d754d4186d1d15b972ec5c99f213bc8
Jeremy <jeremy.dejoux@laplateforme.io>
2021-02-08 11:56:02 +0100

    Ajout de page et de style

--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <?php
-    session_start();
+session_start();
 
-    require_once 'functions/functions.php';
-    require_once 'classes/Autoloader.php';
-    
-    Autoloader::register();
+require_once 'functions/functions.php';
+require_once 'classes/Autoloader.php';
+Autoloader::register();
 
-    if (isset($_REQUEST['difficulty'])) {
-        $difficulty = $_SESSION['difficulty'] = intval(htmlentities($_REQUEST['difficulty']));
-        $deck = new Deck($difficulty);
-        $game = new Memory($deck->getObjectDeck());
-        $game->setDifficulty($difficulty);
-        $_SESSION['game'] = serialize($game);
-        unset($_SESSION['difficulty']);
-        header('Location: game.php');
-        return;
-    }
+if (isset($_REQUEST['difficulty'])) {
+    $difficulty = $_SESSION['difficulty'] = intval(htmlentities($_REQUEST['difficulty']));
+    $deck = new Deck($difficulty);
+    $game = new Memory($deck->getObjectDeck());
+    $game->setDifficulty($difficulty);
+    $_SESSION['game'] = serialize($game);
+    unset($_SESSION['difficulty']);
+    header('Location: game.php');
+    return;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles/styleG.css">
-        <title>choix de la difficulté</title>
-    </head>
-    <body>
-        <header>
-            <ul>
-                <li><a href="index.php">index</a></li>
-                <li><a href="game.php">Jeu</a></li>
-                <li><a href="deconnexion.php">Deconnexion</a></li>
-            </ul>
-        </header>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/index.css">
+    <title>choix de la difficulté</title>
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="index.php">index</a></li>
+            <li><a href="game.php">Jeu</a></li>
+            <li><a href="deconnexion.php">Deconnexion</a></li>
+        </ul>
+    </header>
+
+
+    <main>
+
         <form action="" method='GET'>
             <label for="diff">nombre de paires</label>
             <select name="difficulty" id="diff">
@@ -49,5 +54,11 @@
             </select>
             <input type="submit" value="GO">
         </form>
-    </body>
+
+    </main>
+
+
+    <footer></footer>
+</body>
+
 </html>
