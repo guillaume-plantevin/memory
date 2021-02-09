177a8a80b78a11dee1bbac5b9f2a2dfcce3331ef
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-02-09 12:23:09 +0100

    Re change path

--- pages/inscription.php
+++ pages/inscription.php
@@ -10,7 +10,9 @@ $path_index = '../index.php';
 $path_profil= 'profil.php';
 $path_inscription = 'inscription.php';
 $path_connexion = 'connexion.php';
-$path_wall = 'WallOfFame.php?d=' . $_SESSION['difficulty'];
+if (isset($_SESSION['difficulty'])) {
+    $path_wall = 'pages/WallOfFame.php?d=' . $_SESSION['difficulty'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
